c66c5d0efd0df7766a718b402d50d3489d8484b4
Nilton Fernandes <niltonfernandesj@gmail.com>
2022-11-03 18:29:20 -0300

    All files final commit

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const request = require('request');
 const convert = require('xml-js');
 
-request('http://www.google.com', function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+// AddInteger
+request('https://www.crcind.com/csp/samples/SOAP.Demo.cls?soap_method=AddInteger&Arg1=1&Arg2=1', function (error, response, body) {
+    const convertedBody = JSON.parse(convert.xml2json(body, {compact: true, spaces: 1}));
+    const addIntegerResponse = convertedBody["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["AddIntegerResponse"]["AddIntegerResult"]._text;
+    console.log("\n\nAddIntegerResponse: " + addIntegerResponse);
+});
+
+// DivideInteger
+request('https://www.crcind.com/csp/samples/SOAP.Demo.cls?soap_method=DivideInteger&Arg1=1&Arg2=1', function (error, response, body) {
+    const convertedBody = JSON.parse(convert.xml2json(body, {compact: true, spaces: 1}));
+    const addIntegerResponse = convertedBody["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["DivideIntegerResponse"]["DivideIntegerResult"]._text;
+    console.log("\n\nDivideIntegerResponse: " + addIntegerResponse);
+});
+
+// FindPerson
+request('https://www.crcind.com/csp/samples/SOAP.Demo.cls?soap_method=FindPerson&id=50', function (error, response, body) {
+    const convertedBody = JSON.parse(convert.xml2json(body, {compact: true, spaces: 1}));
+    const addIntegerResponse = convertedBody["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["FindPersonResponse"]["FindPersonResult"];
+    console.log("\n\nFindPersonResponse: " + JSON.stringify(addIntegerResponse));
 });
